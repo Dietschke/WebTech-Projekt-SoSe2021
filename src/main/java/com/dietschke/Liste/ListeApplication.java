bf44a90cc6559f2ced55ab459930fd7ea9b72c8e
Dietschke <maximilian.dietschke@Student.htw-berlin.de>
2021-05-09 19:18:00 +0200

    Milestone II -Bean

--- src/main/java/com/dietschke/Liste/ListeApplication.java
+++ src/main/java/com/dietschke/Liste/ListeApplication.java
@@ -18,33 +18,5 @@ public class ListeApplication {
 		SpringApplication.run(ListeApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner demo(ListeRepository repository) {
-		return (args) -> {
 
-			repository.save(new Liste(1L,"Kuchen", "Schokokuchen von Oma"));
-			repository.save(new Liste(2L,"Burger", "Lieblingsburger"));
-
-
-			log.info("Liste found with findAll():");
-			log.info("-------------------------------");
-			for (Liste liste : repository.findAll()) {
-				log.info(liste.toString());
-			}
-			log.info("");
-
-			Liste liste = repository.findById(1L);
-			log.info("Liste found with findById(1L):");
-			log.info("--------------------------------");
-			log.info(liste.toString());
-			log.info("");
-
-			log.info("Liste found with findByTitle('Kuchen'):");
-			log.info("--------------------------------------------");
-			repository.findByTitle("Kuchen").forEach(kuchen -> {
-				log.info(kuchen.toString());
-			});
-			log.info("");
-		};
-	}
 }
