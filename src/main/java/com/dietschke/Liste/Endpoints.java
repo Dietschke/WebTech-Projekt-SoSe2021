ffcbe921f189457fc47bd14455879ca44b45107b
Dietschke <maximilian.dietschke@Student.htw-berlin.de>
2021-06-26 21:33:59 +0200

    Heroku-Deploy ohne Okta

--- src/main/java/com/dietschke/Liste/Endpoints.java
+++ src/main/java/com/dietschke/Liste/Endpoints.java
@@ -1,10 +1,10 @@
-package com.dietschke.Liste;
-
-public class Endpoints {
-    public static class Site{
-        public static final String START ="/";
-        public static final String ALL ="/all";
-        public static final String VUE ="/vue";
-        public static final String INDEX ="/index";
-    }
-}
+//package com.dietschke.Liste;
+//
+//public class Endpoints {
+//    public static class Site{
+//        public static final String START ="/";
+//        public static final String ALL ="/all";
+//        public static final String VUE ="/vue";
+//        public static final String INDEX ="/index";
+//    }
+//}
--- src/main/java/com/dietschke/Liste/SecurityConfig.java
+++ src/main/java/com/dietschke/Liste/SecurityConfig.java
@@ -1,40 +1,40 @@
-package com.dietschke.Liste;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.websocket.Endpoint;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception{
-        http
-                .authorizeRequests()
-                .antMatchers(
-                        HttpMethod.GET,
-                        Endpoints.Site.START,
-                        Endpoints.Site.INDEX,
-                        Endpoints.Site.VUE,
-                        Endpoints.Site.ALL
-                ).permitAll()
-                .antMatchers(
-                        "/css/**",
-                        "images/**",
-                        "/js/**"
-                ).permitAll()
-                .anyRequest().authenticated()
-
-                .and()
-                .logout().logoutSuccessUrl("/")
-                .and()
-                .oauth2Client()
-                .and()
-                .oauth2Login();
-    }
-}
+//package com.dietschke.Liste;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//import javax.websocket.Endpoint;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception{
+//        http
+//                .authorizeRequests()
+//                .antMatchers(
+//                        HttpMethod.GET,
+//                        Endpoints.Site.START,
+//                        Endpoints.Site.INDEX,
+//                        Endpoints.Site.VUE,
+//                        Endpoints.Site.ALL
+//                ).permitAll()
+//                .antMatchers(
+//                        "/css/**",
+//                        "images/**",
+//                        "/js/**"
+//                ).permitAll()
+//                .anyRequest().authenticated()
+//
+//                .and()
+//                .logout().logoutSuccessUrl("/")
+//                .and()
+//                .oauth2Client()
+//                .and()
+//                .oauth2Login();
+//    }
+//}
